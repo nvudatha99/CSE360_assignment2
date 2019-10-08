c9fbfcb4aeacf949681ecb6942a67afd9ae23a00
nvudatha99 <nikhil@10.0.0.7>
2019-10-08 14:20:14 -0700

    INITIAL VERSION

--- 360hw2/src/cse360assign2/AddingMachine.java
+++ 360hw2/src/cse360assign2/AddingMachine.java
@@ -1,28 +1,61 @@
 package cse360assign2;
 
+/**
+ * 
+ * @author Nikhil Vudatha
+ * Date: 10/9/2019
+ * CSE360 M 70641
+ * Description: This program performs and displays the operations it does on integers.
+ *
+ */
+
 public class AddingMachine {
 	
+	/**
+	 *  a private total variable to keep track of total computed.
+	 */
 	private int total;
 	
+	
 	public AddingMachine() {
-		total = 0;  // not needed - included for clarity
+		// not needed - included for clarity 
+		total = 0;  
 		
 	}
 	
+	/**
+	 * this int function returns the amount saved in total after operations are computed
+	 * @return total
+	 */
 	public int getTotal () {
 		return 0;
 		
 	}
+	/**
+	 * takes in an integer value and performs addition
+	 * @param value
+	 */
 	public void add (int value) {
 		
 	}
+	/**
+	 * takes in an integer value and performs subtraction
+	 * @param value
+	 */
 	public void subtract (int value) {
 		
 	}
+	/**
+	 * this method displays the operations that have been computed in a string format
+	 * @return string
+	 */
 	public String toString () {
 		return "";
 		
 	}
+	/**
+	 * this method clears the sequence of operations stored in the toString and sets total to 0
+	 */
 	public void clear() {
 		
 	}
